26404c9cb74a110cc09e774fed0e32929650526d
Mohd Zeya Iqbal Khan <zeyakhan.cse@gmail.com>
2021-10-17 18:29:28 +0530

    TestCase 3

--- StringCalculator/src/calculator/StringCalculatorShould.java
+++ StringCalculator/src/calculator/StringCalculatorShould.java
@@ -16,5 +16,9 @@ public class StringCalculatorShould {
 	public void string_with_single_number_should_return_number_as_int() {
 		assertEquals(1,stringCalculator.add("1"));
 	}
+	@Test
+	public void string_with_double_number_should_return_sum_of_numbers_as_int() {
+		assertEquals(3,stringCalculator.add("1,2"));
+	}
 
 }
